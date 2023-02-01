e4288cb4ca5e7ba6a9dfd6eb609d5d80b2980e43
Justen Schilinski <justenschilinski34@hotmail.com>
2023-01-31 21:35:05 -0500

    Installed bootstrap and added card element

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Card from 'react-bootstrap/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <Card bg={'warning'}>Here is some text</Card>
     </div>
   );
 }
