5eab242c6e4bfe1741cd8c04259783420d3c6ec7
Justen Schilinski <justenschilinski34@hotmail.com>
2023-02-02 15:53:55 -0500

    Added remaining buttons and customized the layout of all elements

--- src/App.js
+++ src/App.js
@@ -2,31 +2,80 @@ import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Card bg={'warning'} style={{ width: 'calc(22rem + 1vmin)', height: '24rem'}}>
+      <Card style={{ width: 'calc(22rem + 1vmin)', height: '24rem'}}>
         <Row>
-          <Col xs={12}>Display Text</Col>
+          <Col xs={12}>
+            <Container id="display">Display Text</Container>
+          </Col>
         </Row>
         <Row>
           <Col xs={6}>
-            <Button style={{width: '100%'}}>AC</Button>
+            <Button size="lg" variant="danger">AC</Button>
           </Col>
           <Col xs={3}>
-            <Button>/</Button>
+            <Button size="lg" variant="dark">/</Button>
           </Col>
           <Col xs={3}>
-            <Button>X</Button>
+            <Button size="lg" variant="dark">x</Button>
           </Col>
         </Row>
-        <Row></Row>
-        <Row></Row>
-        <Row></Row>
-        <Row></Row>
+        <Row>
+          <Col xs={3}>
+            <Button size="lg" variant="light">7</Button>
+          </Col>
+          <Col xs={3}>
+            <Button size="lg" variant="light">8</Button>
+          </Col>
+          <Col xs={3}>
+            <Button size="lg" variant="light">9</Button>
+          </Col>
+          <Col xs={3}>
+            <Button size="lg" variant="dark">-</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={3}>
+            <Button size="lg" variant="light">4</Button>
+          </Col>
+          <Col xs={3}>
+            <Button size="lg" variant="light">5</Button>
+          </Col>
+          <Col xs={3}>
+            <Button size="lg" variant="light">6</Button>
+          </Col>
+          <Col xs={3}>
+            <Button size="lg" variant="dark">+</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={3}>
+            <Button size="lg" variant="light">1</Button>
+          </Col>
+          <Col xs={3}>
+            <Button size="lg" variant="light">2</Button>
+          </Col>
+          <Col xs={3}>
+            <Button size="lg" variant="light">3</Button>
+          </Col>
+          <Col xs={3}>
+            <Button id="equals" size="lg">=</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={6}>
+            <Button size="lg" variant="light">0</Button>
+          </Col>
+          <Col xs={3}>
+            <Button size="lg" variant="light">.</Button>
+          </Col>
+        </Row>
       </Card>
     </div>
   );
